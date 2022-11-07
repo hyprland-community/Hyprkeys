0210688563a65e77f2e4ce41193220489acd6b4e
NotAShelf <itsashelf@gmail.com>
2022-11-07 20:58:20 +0300

    dev: indev feature for combining command flags

---
+++ util/cli/flags.go
@@ -0,0 +1,8 @@
+package flags
+
+// TODO
+// If any flags are passed, store all of them in a map
+// We will then check this variable to see which flags
+// were passed, then maybe prevent some flags from being
+// passed together
+// TODO
