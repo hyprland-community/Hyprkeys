f0f749791eedb1a28a55539fb79c301c3ae3a458
jjohnstondev <jakester620@gmail.com>
2023-01-04 00:45:40 -0800

    readd markdown

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"path/filepath"
 	"runtime/debug"
+	"strings"
 
 	"github.com/pborman/getopt"
 
@@ -67,7 +68,6 @@ func main() {
 		}
 	}
 	if flags.Json {
-
 		out, err := json.MarshalIndent(configValues, "", " ")
 		if err != nil {
 			log.Println(err.Error())
@@ -75,4 +75,67 @@ func main() {
 		}
 		fmt.Println(string(out))
 	}
+	if flags.Markdown {
+		md := keybindsToMarkdown(configValues.KeyboardBinds, configValues.MouseBinds)
+		println("| Keybind | Dispatcher | Command |")
+		println("|---------|------------|---------|")
+		for _, row := range md {
+			println(row)
+		}
+	}
+}
+
+// Pass both kbKeybinds and mKeybinds to this function
+func keybindsToMarkdown(kbKeybinds, mKeybinds []string) []string {
+	var markdown []string
+	for _, keybind := range kbKeybinds {
+		keybind = strings.TrimPrefix(keybind, "bind=")
+
+		// Split "keybind" into a slice of strings
+		// based on the comma delimiter
+		keybindSlice := strings.SplitN(keybind, ",", 4)
+
+		// Trim whitespace from keybindSlice[1] to keybindSlice[3]
+		keybindSlice[1] = strings.TrimSpace(keybindSlice[1])
+		keybindSlice[2] = strings.TrimSpace(keybindSlice[2])
+		keybindSlice[3] = strings.TrimSpace(keybindSlice[3])
+
+		// Print the keybind as a markdown table row
+
+		// Check if keybindSlice is empty
+		// Trim the whitespace and "+" if it is
+		if keybindSlice[0] == "" {
+			keybindSlice[1] = strings.TrimSpace(keybindSlice[1])
+			markdown = append(markdown, "| <kbd>"+keybindSlice[1]+"</kbd> | "+keybindSlice[2]+" | "+keybindSlice[3]+" |")
+
+		} else {
+			markdown = append(markdown, "| <kbd>"+keybindSlice[0]+" + "+keybindSlice[1]+"</kbd> | "+keybindSlice[2]+" | "+keybindSlice[3]+" |")
+		}
+	}
+
+	for _, keybind := range mKeybinds {
+		keybind = strings.TrimPrefix(keybind, "bindm=")
+
+		// Split "keybind" into a slice of strings
+		// based on the comma delimiter
+		keybindSlice := strings.SplitN(keybind, ",", 3)
+
+		// Trim whitespace from keybindSlice[1] to keybindSlice[2]
+		keybindSlice[1] = strings.TrimSpace(keybindSlice[1])
+		keybindSlice[2] = strings.TrimSpace(keybindSlice[2])
+
+		// Print the keybind as a markdown table row
+
+		// Check if keybindSlice[0] is null
+		// Trim the whitespace and "+" if it is
+		if keybindSlice[0] == "" {
+			markdown = append(markdown, "| <kbd>"+keybindSlice[1]+"</kbd> | | "+keybindSlice[2]+" |")
+		} else {
+			// put "| |" inbetween the keybindSlice[0] and keybindSlice[1]
+			markdown = append(markdown, "| <kbd>"+keybindSlice[0]+" + "+keybindSlice[1]+"</kbd> | | "+keybindSlice[2]+" |")
+		}
+
+	}
+
+	return markdown
 }
