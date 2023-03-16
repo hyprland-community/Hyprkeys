07bd97225e24df0962cc6d08b22c349a98593f4b
NotAShelf <itsashelf@gmail.com>
2023-03-16 12:08:10 +0300

    dev: initial work on nix packaging

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Hyprkeys, a keybind inspection utility for Hyprland";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.gomod2nix.url = "github:nix-community/gomod2nix";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    gomod2nix,
+  }: (
+    flake-utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [gomod2nix.overlays.default];
+      };
+    in {
+      packages.default = pkgs.callPackage ./nix {};
+      devShells.default = import ./nix/shell.nix {inherit pkgs;};
+    })
+  );
+}
